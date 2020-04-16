dc63c63381b58303c711f1bda6a76d7467d35f3c
Shereen789 <irfanashereen@msitprogram.net>
2020-04-16 16:54:07 +0530

    task 2

--- project1/application.py
+++ project1/application.py
@@ -1,13 +1,15 @@
 import os
-# import time
+import datetime
+from models import *
+from create import *
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 from flask import Flask, render_template, request
-# from flask import session
+from flask import session
 from flask_session import Session
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -16,6 +18,8 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+
+
 Session(app)
 
 # Set up database
@@ -39,25 +43,33 @@ def register():
     return render_template('register.html')
 
 
-@app.route("/hello", methods=["POST"])
-def hello():
-    name = request.form.get("Email")
-    pwd = request.form.get("password")
-    print(name+"#"+pwd)
-    return "Hello "+name.split('@')[0]+"! You have successfully registered"
-
-
 # @app.route("/hello", methods=["POST"])
 # def hello():
 #     name = request.form.get("Email")
 #     pwd = request.form.get("password")
-#     ts = time.gmtime()
-#     tstamp = time.strftime("%c", ts)
-#     try:
-#         db.execute("INSERT INTO user_database(username,pword,creation_timestamp)
-# VALUES(:username,:pword,:creation_timestamp)", {
-#             "username": name, "pword": pwd, "creation_timestamp": tstamp})
-#         db.commit()
-#         return "Hello "+name.split('@')[0]+"! You have successfully registered"
-#     except:
-#         return "Failed to Register"
+#     print(name+"#"+pwd)
+#     return "Hello "+name.split('@')[0]+"! You have successfully registered"
+
+
+@app.route("/display", methods=["POST"])
+def display():
+    uname = request.form.get("Email")
+    pwd = request.form.get("password")
+    tstamp = datetime.datetime.now()
+    print(uname, pwd, tstamp)
+    try:
+        db.execute("INSERT INTO userdata(username, passwords, creationstamp) VALUES (:username, :passwords, :creationstamp)",
+                   {"username": uname, "passwords": pwd, "creationstamp": tstamp})
+        db.commit()
+        return "Hello "+uname.split('@')[0]+"! You have successfully registered"
+    except:
+        return "Hello "+uname.split('@')[0] + "! Failed to Register"
+
+
+@app.route("/admin")
+def admin():
+    user_data = Userdata.query.all()
+    lis = []
+    for data in user_data:
+        lis.append(data)
+    return lis
