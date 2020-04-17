d3e5be2c66d77adc5609b8e78d164054e859ae68
Shereen789 <irfanashereen@msitprogram.net>
2020-04-17 19:46:07 +0530

    Project_1: 17-04-2020_ Session login logout

--- project1/models.py
+++ project1/models.py
@@ -7,4 +7,12 @@ class Userdata(db.Model):
     __tablename__ = "userdata"
     username = db.Column(db.String, primary_key=True)
     passwords = db.Column(db.String, nullable=False)
-    creationstamp = db.Column(db.String, nullable=False)
+    creationstamp = db.Column(db.DateTime, nullable=False)
+
+
+class Books(db.Model):
+    __tablename__ = "booksdata"
+    isbn = db.Column(db.String, primary_key=True)
+    title = db.Column(db.String)
+    author = db.Column(db.String)
+    year = db.Column(db.String)
