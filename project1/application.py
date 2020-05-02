af33a201d7039f674da0cfede42374e949b95845
Shereen789 <irfanashereen@msitprogram.net>
2020-05-02 15:56:51 +0530

    Search Feature with Js

--- project1/application.py
+++ project1/application.py
@@ -12,10 +12,6 @@ from flask_session import Session
 from flask import Flask, jsonify, json
 from flask_cors import CORS, cross_origin
 
-
-# cors = CORS(app, resources={r"*": {"origins": "*"}})
-# app = Flask(__name__)
-
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
@@ -48,12 +44,6 @@ def register():
     if request.method == "POST":
         uname = request.form.get("Email")
         pwd = request.form.get("password")
-        if uname == "" and pwd == "":
-            return render_template('register.html', message="USERNAME AND PASSWORD CAN'T BE EMPTY. TRY AGAIN")
-        if uname == "":
-            return render_template('register.html', message="USERNAME CAN'T BE EMPTY. TRY AGAIN")
-        if pwd == "":
-            return render_template('register.html', message="PASSWORD CAN'T BE EMPTY. TRY AGAIN")
         if validate_user(uname):
             return render_template('login.html', message="USER ALREADY EXISTS. LOGIN WITH THE CREDENTIALS")
 
@@ -72,61 +62,23 @@ def register():
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    # session.clear()
     if request.method == "POST":
         uname = request.form.get("Email")
         pwd = request.form.get("password")
-        # if uname == "" and pwd == "":
-        #     return render_template('login.html', message="USERNAME AND PASSWORD CAN'T BE EMPTY. TRY AGAIN")
-        # if uname == "":
-        #     return render_template('login.html', message="USERNAME CAN'T BE EMPTY. TRY AGAIN")
-        # if pwd == "":
-        #     return render_template('login.html', message="PASSWORD CAN'T BE EMPTY. TRY AGAIN")
-        # if not validate_user(uname):
-        #     return render_template('register.html', message="USER DOESN'T EXISTS. REGISTER")
-        # elif not validate(uname, pwd):
-        #     return render_template('login.html', message="PASSWORD NOT MATCHED. TRY AGAIN")
-
+        if not validate_user(uname):
+            return render_template('register.html', message="User doesn't Exist. Register yourself")
+        elif not validate(uname, pwd):
+            return render_template('login.html', message="Ops!. Password not matched. Try Again")
         session['Email'] = request.form['Email']
         return render_template('profile.html')
-
     else:
         return render_template('login.html')
-
-        # return render_template('profile.html')
 
 
 @app.route("/logout")
 def logout():
     session.clear()
     return render_template('login.html', message="Please Log In using your Credentials")
-
-# Basic search directing to new HTML PAGE
-# @app.route("/search", methods=["POST"])
-# def search():
-#     if request.method == "POST":
-#         search_by = request.form.get("search_with").strip()
-#         search_text = "%"+request.form.get("search_text").strip()+"%"
-#         print(search_by, search_text)
-#         if search_by == "1":
-#             results = db.query(Books).filter(
-#                 Books.author.like(search_text)).all()
-#         if search_by == "2":
-#             results = db.query(Books).filter(
-#                 Books.isbn.like(search_text)).all()
-#         if search_by == "3":
-#             results = db.query(Books).filter(
-#                 Books.title.like(search_text)).all()
-#         if results != None:
-#             return render_template('search.html', results=results)
-#         else:
-#             return "No such Details Found"
-# -----------------------------------------------------------------------------------
-# WIth AJAX IMPLEMENTATION
-
-# @app.route("/search", methods=["GET"])
-# def search():
-#     return render_template('profile.html')
 
 
 @app.route("/api/search/<data>")
@@ -191,3 +143,30 @@ def get_data(i, text):
 
 #     # re = json.load(j)
 #     # print(type(re))
+
+# Basic search directing to new HTML PAGE
+# @app.route("/search", methods=["POST"])
+# def search():
+#     if request.method == "POST":
+#         search_by = request.form.get("search_with").strip()
+#         search_text = "%"+request.form.get("search_text").strip()+"%"
+#         print(search_by, search_text)
+#         if search_by == "1":
+#             results = db.query(Books).filter(
+#                 Books.author.like(search_text)).all()
+#         if search_by == "2":
+#             results = db.query(Books).filter(
+#                 Books.isbn.like(search_text)).all()
+#         if search_by == "3":
+#             results = db.query(Books).filter(
+#                 Books.title.like(search_text)).all()
+#         if results != None:
+#             return render_template('search.html', results=results)
+#         else:
+#             return "No such Details Found"
+# -----------------------------------------------------------------------------------
+# WIth AJAX IMPLEMENTATION
+
+# @app.route("/search", methods=["GET"])
+# def search():
+#     return render_template('profile.html')
